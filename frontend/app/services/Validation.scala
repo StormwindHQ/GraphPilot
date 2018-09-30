ea3e1e4655b38e0e4dc97e726a557380df323790
Jason Shin <visualbbasic@gmail.com>
2018-09-30 23:22:15 +1000

    chore: implementing required check and renaming

--- frontend/app/services/Validation.scala
+++ frontend/app/services/Validation.scala
@@ -1,32 +1,47 @@
 package services
 import javax.inject.Inject
-import java.nio.file.{Paths}
-import play.api.libs.json._
+import java.nio.file.Paths
+import play.api.libs.json.{ JsValue, Json, JsUndefined }
+import scala.xml.dtd.ValidationException
 
 class Validation {
-  def validateTaskInputs(
+
+  /**
+    * Given a JsValue payload, and a schema.json to pick up;
+    * It will compare the payoad and process validation accordingly
+    * 1. Check required = if schema.required == true, then payload["name"] should not be undefined
+    * @param appName
+    * @param taskType
+    * @param taskName
+    * @param inputs
+    * @return
+    */
+  def validateTaskPayload(
     appName: String,
     taskType: String,
     taskName: String,
-    inputs: JsValue = null,
-  ): Unit = {
+    payload: JsValue, // can be either env or inputs
+    payloadType: String = "schema",
+  ): Boolean = {
     val fs = new FileSystem
     val pwd = System.getProperty("user.dir")
-    val filePath = Paths.get(pwd, "..", "tasks", appName, taskType, taskName, "schema.json").toString
+    val filePath = Paths.get(pwd, "..", "tasks", appName, taskType, taskName, s"${payloadType}.json").toString
     val source: String = fs.readFileAsString(filePath)
     val schema: JsValue = Json.parse(source)
     val schemaInputs = (schema \ "inputs").as[List[JsValue]]
 
     schemaInputs.foreach(x => {
-      println(x)
+      // Loop to check values of the given inputs
       val name = (x \ "name").as[String]
       val xType = (x \ "type").as[String]
       val required = (x \ "required").as[Boolean]
-      println("yo", (inputs \ name))
-      // Check required
+
+      // 1. If the current target is required, inputs should have the value
       if (required && (inputs \ name).isInstanceOf[JsUndefined]) {
-        println("undefined!", name)
+        throw new ValidationException(f"${name} is a required value")
       }
+
+      // 2.
     })
     true
   }
