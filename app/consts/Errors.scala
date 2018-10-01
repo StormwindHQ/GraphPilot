62432ab09267294367a0c9e5f4f70e94071fea69
Jason Shin <visualbbasic@gmail.com>
2018-10-01 16:10:14 +1000

    chore: adding comments on each exceptions

--- app/consts/Errors.scala
+++ app/consts/Errors.scala
@@ -1,7 +1,7 @@
 package consts
 
 /**
-  *
+  * When required field is not present
   * @param fieldName
   * @param cause
   */
@@ -10,18 +10,22 @@ final case class FieldRequiredException(
   private val cause: Throwable = None.orNull)
   extends Exception(s"${fieldName} is required")
 
+/**
+  * When a field type is string but the value is something else
+  * @param fieldName
+  * @param cause
+  */
 final case class FieldStringException(
   private val fieldName: String,
   private val cause: Throwable = None.orNull)
   extends Exception(s"${fieldName} should be a string")
 
+/**
+  * When a field type is an array of string but the value is something else
+  * @param fieldName
+  * @param cause
+  */
 final case class FieldStringArrayException(
   private val fieldName: String,
   private val cause: Throwable = None.orNull)
   extends Exception(s"${fieldName} should be an array of string")
-
-object Errors {
-  val MSG_REQUIRED = "%s is required"
-  val MSG_NOT_STRING = "%s is not a string"
-  val MSG_NOT_ARRAY_STRING = "%s is not a string array"
-}
