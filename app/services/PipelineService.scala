41abc94a590e2e3753db97a49622b2d84d945c8c
Jason Shin <visualbbasic@gmail.com>
2018-10-08 17:20:48 +1100

    adding findEdgeById

--- app/services/PipelineService.scala
+++ app/services/PipelineService.scala
@@ -10,6 +10,27 @@ import play.api.libs.json._
   */
 
 class PipelineService {
+
+  /**
+    * Finds an edge in a edges list that matches both from and to
+    *
+    * @example
+    * val inputs: JsValue = Json.parse("""
+    * {
+    *   "edges": [ { "from": "task_1", "to": "task_2" }, { "from": "task_2", "to": "task_3" } ]
+    * }
+    * """)
+    *
+    * findEdgeById((inputs \ "edges").as[List[JsValue]], "task_1", "task_2)
+    * // returns { "from": "task_1", "to": "task_2" }
+    *
+    * @param edges
+    * @param from
+    * @param to
+    */
+  def findEdgeById(edges: List[JsValue], from: String, to: String): Unit = {
+    edges.filter((x: JsValue) => (x \ "from").as[String] == from && (x \ "to").as[String] == to).head
+  }
   /**
     * Creates a pipeline according to the pipelineScript. It should create multidimentional sequences and
     * track the pipeline as a database entity, which can be later retrieved, deleted or updated.
@@ -18,7 +39,9 @@ class PipelineService {
   def create(pipelineScript: JsValue): Boolean = {
     val nodes = (pipelineScript \ "nodes").as[List[JsValue]]
     val edges = (pipelineScript \ "edges").as[List[JsValue]]
-    print("nodes", nodes, edges)
+    val triggers = nodes.filter((x: JsValue) => (x \ "taskType").as[String] == "trigger")
+    println("checking triggers", triggers)
+
     true
   }
 }
