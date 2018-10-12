9e92d435073e22279b63bf7b7cca381fc8a37099
Jason Shin <visualbbasic@gmail.com>
2018-10-12 17:44:27 +1100

    chore: remobing the json blog from graphutil

--- app/utils/GraphUtil.scala
+++ app/utils/GraphUtil.scala
@@ -3,57 +3,6 @@ package utils
 import play.api.libs.json._
 
 class GraphUtil {
-  def test(): Unit ={
-    val inputs: JsValue = Json.parse("""
-      {
-       "nodes": [
-         { "id": "task_1", "guid": "trigger_12938x12938", "taskApp": "github", "taskType": "trigger", "taskName": "on_wiki_update", "chart": { "x": 12, "y": 39 } },
-         { "id": "task_2", "guid": "action_12983xcv", "taskApp": "github", "taskType": "action", "taskName": "create_issue", "chart": { "x": 55, "y": 203 } },
-         { "id": "task_3", "guid": "action_3432aa", "taskApp": "conditions", "taskType": "condition", "taskName": "wait", "chart": { "x": 232, "y": 111 } },
-         { "id": "task_4", "guid": "action_634643asd1", "taskApp": "github", "taskType": "action", "taskName": "create_commit", "chart": { "x": 312, "y": 11 } },
-         { "id": "task_5", "guid": "trigger_3928429xx", "taskApp": "discord", "taskType": "trigger", "taskName": "on_new_message", "chart": { "x": 91, "y": 211 } } ],
-       "edges": [
-         {
-           "from": "task_1",
-           "to": "task_2",
-           "payload": {
-             "title": "Creating a new issue for fun!",
-             "body": "${task_1.createdDate} ${task_1.title} was updated just now!"
-           }
-         },
-         {
-           "from": "task_2",
-           "to": "task_3",
-           "payload": {
-             "title": "Creating a new issue for fun!",
-             "body": "${task_1.createdDate}",
-             "delay": 5000
-           }
-         },
-         {
-           "from": "task_2",
-           "to": "task_4"
-         },
-         {
-           "from": "task_3",
-           "to": "task_4",
-           "payload": {
-             "delay": 5000,
-             "message": "As a result of wiki article ${task_1.title} update, now the system will make a new commit"
-           }
-         },
-         {
-           "from": "task_5",
-           "to": "task_4",
-           "payload": {
-             "message": "Making a new commit out of a new message \"${task_5.message}\" from discord!"
-           }
-         }
-       ]
-      }
-    """)
-    println(getAllPaths(inputs, "task_1"))
-  }
 
   /**
     * Retreives a simple list of direct successors' IDs from a node
